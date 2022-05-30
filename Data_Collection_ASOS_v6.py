b425e310e75938af3350ad0301befa6c36ab7e5a
Jazzmin Washington <102431019+Jazzmin-Washington@users.noreply.github.com>
2022-05-30 17:57:09 +0100

    Added return values for unit testing

--- Data_Collection_ASOS_v6.py
+++ Data_Collection_ASOS_v6.py
@@ -38,14 +38,14 @@ if __name__ == "__main__":
             self.n_pages = 5
             self.actions = ActionChains(self.driver)
             self.delay = 10
-
-
-        # Clicks the accept cookies button on the selenium driver
+     # Clicks the accept cookies button on the selenium driver
         def load_and_accept_cookies(self):
             self.driver.implicitly_wait(10)
             accept_cookies_button = WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH,'//*[@id="onetrust-accept-btn-handler"]')))
+            self.driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack', 'True');});",accept_cookies_button)
             accept_cookies_button.click()
             time.sleep(2)
+            return print(accept_cookies_button.get_attribute("automationTrack"))
 
         # Collects the links on all the loaded products and saved to list
         def get_product_links(self):
@@ -59,10 +59,11 @@ if __name__ == "__main__":
                     a_tag = articles.find_element_by_tag_name('a')
                     link = a_tag.get_attribute('href')
                     self.shop_link_list.append(link)
-            
+
             # Confirms the necessary links have been collected       
             print(f'There are {len(self.shop_link_list)} in this link list')
-        
+            return self.shop_link_list
+
         # Selects the 'load more' button of the sale website to load more products
         def load_more_products(self):
             for i in range(6):
@@ -73,6 +74,7 @@ if __name__ == "__main__":
                 if self.load_pages == 0:
                     self.actions.move_to_element(self.load_more_button)
                     self.load_more_button.click()
+                    self.driver.execute_script("var ele = arguments[0];ele.addEventListener('click', function() {ele.setAttribute('automationTrack', 'True');});",self.load_more_button)
                     self.driver.implicitly_wait(10)
                     self.load_pages +=1
 
@@ -85,6 +87,7 @@ if __name__ == "__main__":
                     self.actions.move_to_element(self.load_more_button)
                     self.load_more_button.click()
                     self.driver.implicitly_wait(5)
+            return print(self.load_more_button.get_attribute("automationTrack"))
 
         # Collects the data from the each of the collected links(product code, sale price etc.)
         def get_product_data(self):
@@ -101,8 +104,8 @@ if __name__ == "__main__":
                     self.popup.click()
                 except (ElementNotInteractableException, NoSuchElementException):
                     pass
-                
-              # Selects the show more button to expose the relevant data for scraping  
+
+                # Selects the show more button to expose the relevant data for scraping  
                 try:
                     self.driver.implicitly_wait(10)
                     self.show_more_button = self.driver.find_element_by_xpath('//a[@class="show"]') 
@@ -110,7 +113,7 @@ if __name__ == "__main__":
                     self.show_more_button.click()
                 except NoSuchElementException:
                     pass
-                
+
                 # Collects the product id for each of the products
                 try:
                     self.product_id = self.driver.find_element_by_xpath('//div[@class="product-code"]')
@@ -126,7 +129,7 @@ if __name__ == "__main__":
                     image_src = image.get_attribute('src')
                     self.full_item_list['image'].append(image_src)
                     self.imagesrc_list.append(image_src)
-    
+
                 except NoSuchElementException:
                     self.full_item_list['image'].append('None')
 
@@ -137,12 +140,12 @@ if __name__ == "__main__":
                     self.full_item_list['product_name'].append(product_name)
                 except NoSuchElementException:
                     self.full_item_list['product_name'].append('None')
-                
+
                 #Collects the market price for each of the products
                 try:
                     previous_price = self.driver.find_element_by_xpath('//span[@data-id ="rrp-price"]').text.strip()
                     previous_price = previous_price.replace('RRP', '')
-                    
+
                     #Some of the website use the RRP tag and other used a previous_price tag
                     if previous_price == '':
                         previous_price = self.driver.find_element_by_xpath(('//span[@data-id ="previous-price"]')).text.strip()
@@ -158,7 +161,7 @@ if __name__ == "__main__":
                     self.full_item_list['sale_price'].append(sale_price)
                 except NoSuchElementException:
                     self.full_item_list['sale_price'].append('None')
-                
+
                 # Collects the percentage difference between market and sale price of products
                 try:
                     sale_percentage = self.driver.find_element_by_xpath('//span[@class="product-discount-percent"]').text.strip()
@@ -169,7 +172,7 @@ if __name__ == "__main__":
                 # Collects the color of the items. Some have multiple colors
                 try:
                     colour = self.driver.find_element_by_xpath('//span[@class="product-colour"]').text.strip()
-                    
+
                     # If there are multiple colors instead of sizes then they are accounted for below
                     if colour == '':
                         colour = self.driver.find_element_by_xpath('//div[@data-test-id="colour-size-select"]').text.strip()
@@ -186,7 +189,7 @@ if __name__ == "__main__":
                     self.full_item_list['product_details'].append(ab_description)
                 except NoSuchElementException:
                     self.full_item_list['product_details'].append('None')
-                
+
                 # Collects the sizes for each of the products
                 try:
                     sizes = self.driver.find_element_by_xpath('//select[@data-id="sizeSelect"]').text.strip()
@@ -196,33 +199,37 @@ if __name__ == "__main__":
                     self.full_item_list['sizes'].append(sizes)
                 except NoSuchElementException:
                     self.full_item_list['sizes'].append('None')
-        
+
                 # Added to organize each item by their respective product id
                 self.organized_data  = {self.product_id_num:{'name':[product_name], 'image':[image_src], 'previous_price':[previous_price], 'sale price':[sale_price],'sale_percentage':[sale_percentage], 'color':[colour], 'description':[ab_description], 'sizes':[sizes]}}
+                print(self.full_item_list[i])
                 self.full_product_data.append(self.organized_data)
-       
+            return self.full_item_list, self.full_product_data
+
 
         # Saves the data to either a json file or a pd Dataframe/csv for later    
         def save_data(self):
             index = ['product_id','product_name', 'image', 'previous_price', 'sale_price', 'sale_percentage' 'color', 'product_details', 'sizes']
             org_index = {'product_id':{'product_name', 'image', 'previous_price','sale_price','sale_percentage','color','product_details', 'sizes'}}
-            
+
             with open('ASOS_data/ASOS_Women_data', 'w') as fp:
                 json.dump(self.full_item_list, fp,  indent=4)
 
             with open('ASOS_data/ASOS_Women_Org_data', 'w') as fp:
                 json.dump(self.full_product_data, fp,  indent=4)
-                    
-            
+
+
             df1 = pd.DataFrame.from_dict(self.full_item_list) 
             df1.to_csv (r'ASOS_data/ASOS_Women_Data_csv', index = index, header=True)
-            
+
             df2 = pd.DataFrame.from_dict(self.full_product_data) 
             df2.to_csv (r'ASOS_data/ASOS_Women__Org_Data_csv', index = org_index, header=True)
-        
+
+            if os.path.exists('ASOS_data/ASOS_Women_Data_csv','ASOS_data/ASOS_Women_Org_data' ):
+                return True
+
         # Will download each of the images from their respective image sources for later
         def get_images(self):
-            print(self.imagesrc_list)
             if not os.path.exists('ASOS_data/images'):
                     os.makedirs('ASOS_data/images')
 
@@ -231,13 +238,19 @@ if __name__ == "__main__":
             number = 1
             for i in tqdm(range(len_images), 'Downloading images'):
                 id = self.full_item_list['product_id'][i]
-                if self.full_item_list['product_id'][i] == '':
+                if self.full_item_list['product_id'][i] == '' or 'None':
                     id = number 
                 image = self.imagesrc_list[i]
                 if image == 'None':
                     pass
                 urllib.request.urlretrieve(image, f"ASOS_data/images/ASOS_image_{id}.jpg")
                 number +=1
+                if os.path.exists(f"ASOS_data/images/ASOS_image_{id}.jpg"):
+                    return True
+                else:
+                    return False
+
+
 
         # Final Function that calls all the functions above in the necessary order    
         def scrape_website(self):
