3de3be99e793e6d0e42c071cb1ef4de9ad292cf7
Jazzmin Washington <102431019+Jazzmin-Washington@users.noreply.github.com>
2022-06-06 22:37:37 +0100

    Update test_save_images.py

--- tests/test_save_images.py
+++ tests/test_save_images.py
@@ -10,6 +10,10 @@ from scrapers.Data_Collection_ASOS_small_batch_for_testing import ASOS
 
 class TestSaveImages(unittest.TestCase):
     def setUpClass():
+        ASOS.load_and_accept_cookies()
+        ASOS.nav_to_sale_pg()
+        ASOS.get_product_links()
+        ASOS.get_product_data()
         ASOS.get_images()
   
     def test_image_sources(self):
@@ -17,12 +21,12 @@ class TestSaveImages(unittest.TestCase):
             self.assertTrue(ASOS.imagesrc_list[image] == ASOS.full_item_list['image'][image], 'Image srcs do not match from website scrape')
     
     def test_check_images(self):
-        for image in ASOS.test_images:
-            self.assertTrue(image == True, 'Each image was not downloaded successfully')
+        for i in ASOS.test_images:
+            self.assertTrue(ASOS.test_images[i] == True, 'Each image was not downloaded successfully')
 
     def test_remove_tempfile(self):
-        if os.path.exists('/home/jazz/Documents/AiCore_Projects/Data_Collection_Pipeline/Data-Collection-Pipeline/scrapers/ASOS_data/image'):
-            shutil.rmtree('/home/jazz/Documents/AiCore_Projects/Data_Collection_Pipeline/Data-Collection-Pipeline/scrapers/ASOS_data/image')
+        if os.path.exists('/home/jazz/Documents/AiCore_Projects/Data_Collection_Pipeline/Data-Collection-Pipeline/ASOS_data/image'):
+            shutil.rmtree('/home/jazz/Documents/AiCore_Projects/Data_Collection_Pipeline/Data-Collection-Pipeline/ASOS_data/image')
             
         else:
             assertion = True
@@ -33,5 +37,6 @@ class TestSaveImages(unittest.TestCase):
         ASOS.driver.quit()
 
 if __name__ == '__main__':
-    unittest.main(
+    unittest.main()
+        
         
