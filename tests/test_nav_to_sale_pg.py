27bcc9cc6721f638799254a3e3b1816b8595cce5
Jazzmin Washington <102431019+Jazzmin-Washington@users.noreply.github.com>
2022-06-14 20:59:43 +0100

    Working TestSuite Nav Unit Test

--- tests/test_nav_to_sale_pg.py
+++ tests/test_nav_to_sale_pg.py
@@ -3,25 +3,26 @@
 import selenium
 from selenium.webdriver import Chrome
 import unittest
+import time
 from unittest import TestCase
 from scrapers.Data_Collection_ASOS_small_batch_for_testing import ASOS
 
-class TestNavToTheSalePage(unittest.TestCase):
+class TestFunction_2(unittest.TestCase):
     def setUpClass():
-        ASOS.nav_to_sale_pg()
+        new_scraper = ASOS
+        ASOS._nav_to_sale_pg()
        
 # Unit Test 1: Test whether the load_button works
     def test_webpage_site(self):
         actual = ASOS.driver.current_url
-        expected = 'https://www.asos.com/women/sale/cat/?cid=7046&nlid=ww|sale|shop+sale+by+product|sale+view+all'
+        expected = 'https://www.asos.com/women/sale/cat/?cid=7046&nlid=ww|sale|shop+by+product|sale+view+all'
         self.assertEqual(actual, expected, 'Driver did not go to the sale webpage')
 
 
-    def tearDownClass(self):
-        ASOS.driver.quit()
+    def tearDownClass():
+        print('\n Navigation complete')
+    
 
 if __name__ == '__main__':
     unittest.main()
-        
-       
     
